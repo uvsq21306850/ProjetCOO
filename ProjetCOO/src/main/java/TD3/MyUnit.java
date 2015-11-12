7e2749eac03663613ea0c8ba77f7e77b5f592b90
Utilisateur <idriss.migaou@outlook.fr>
2015-11-12 10:13:41 +0100

    Add ProjetCOO

---
+++ ProjetCOO/src/main/java/TD3/MyUnit.java
@@ -0,0 +1,18 @@
+package TD3;
+/*package TD3;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+*/
+public class MyUnit {
+
+    public String concatenate(String one, String two){
+        return one + two;
+    }
+}
---
+++ ProjetCOO/src/test/java/TD3/MyUnitTest.java
@@ -0,0 +1,55 @@
+package TD3;
+/*
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite; 
+
+
+  //Unit test for simple App.
+ 
+public class AppTest 
+    extends TestCase
+{
+    
+     //Create the test case
+     
+     // @param testName name of the test case
+     
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    
+     // @return the suite of tests being tested
+     
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    
+     // Rigourous Test :-)
+     
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
+
+*/
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testConcatenate() {
+        MyUnit myUnit = new MyUnit();
+
+        String result = myUnit.concatenate("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+}
